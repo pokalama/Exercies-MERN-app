4be46fb2c8023f15b5c6148f92ea56bc92202b64
pokalama <alanisyates96@gmail.com>
2019-11-27 09:47:34 -0500

    adding front end

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/Navbar.component";
+import ExerciseList from "";
+import EditExercise from "";
+import CreateExercise from "";
+import CreateUser from "";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
+}
+
+export default App;
